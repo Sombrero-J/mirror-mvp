98f0ead0b08ae6ada35f9b2dda0a1219930907ad
Weng Chan <wengchan99899.com>
2024-06-21 09:54:48 +0800

    sass working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Mirror from "$lib/components/logo/mirror.ai.svelte";
+</script>
+
+<div class="logo">
+  <Mirror />
+</div>
+
+<div class="upload-container">
+  <label for="imageUpload">
+    <h1>Upload A <span class="highlight">Full Body</span> Image</h1>
+    It works best if the shape of your body is visible
+  </label>
+  <div class="upload-box">
+    <input type="file" id="imageUpload" class="image-input" accept="image/*" />
+    <p class="placeholder">Click To Upload Image</p>
+  </div>
+</div>
+
+<style lang="scss">
+  $border-radius: 0.3rem;
+  
+  .upload-container {
+    outline: 1px solid $color-primary;
+    width: 100%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1.875rem;
+  }
+
+  .upload-box {
+    display: flex;
+    height: 23rem;
+    width: 22rem;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: $border-radius;
+    border: 1.422px solid var(--background-accent-yellow-bolder, #d6960c);
+    background: var(--background-accent-yellow-subtlest, #fff8ea);
+    backdrop-filter: blur(0.9479034543037415px);
+  }
+
+  .image-input {
+    height: 100%;
+    width: 100%;
+    cursor: pointer;
+    opacity: 0;
+    position: absolute;
+  }
+
+  @media (min-width: 1024px) {
+    .upload-container {
+      flex-direction: row;
+    }
+  }
+
+  .logo {
+    outline: 1px solid blue;
+    // display: inline-block;
+    position: absolute;
+    bottom: 0.5rem;
+    margin: 0 auto;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-auto";
+// import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { sveltePreprocess } from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  preprocess: [
+    sveltePreprocess({
+      scss: {
+        prependData: "@use 'src/styles/scss/main.scss' as *;",
+      },
+    }),
+  ],
+
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+  },
 };
 
 export default config;
